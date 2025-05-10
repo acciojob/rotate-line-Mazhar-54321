f7b881345e737e12532c3dcec36bbd3969437d3d
Mazhar-54321 <160312563+Mazhar-54321@users.noreply.github.com>
2025-05-10 12:09:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-//your JS code here. If required.
+const line = document.getElementById('line');
+let rotate=0;
+  
+document.addEventListener("DOMContentLoaded",function(){
+    setInterval(()=>{
+        rotate+=2;
+        if(rotate>360){
+            rotate=0
+        }
+        line.style.transform = `rotate(${rotate}deg)`;
+    },20)
+})
